c7361517c1e8680d03716215211c32a0c3429431
Daniel Probst <daenuprobst@gmail.com>
2023-05-20 21:47:43 +0200

    Updated install instructions

---
+++ install_torch.sh
@@ -0,0 +1,8 @@
+pip install torch torchvision torchaudio
+pip install torch_geometric
+
+python -c 'import torch; print(torch.__version__)'
+
+pip install torch-scatter -f https://data.pyg.org/whl/torch-2.0.1+cu117.html
+pip install torch-sparse -f https://data.pyg.org/whl/torch-2.0.1+cu117.html
+pip install torch-cluster -f https://data.pyg.org/whl/torch-2.0.1+cu117.html

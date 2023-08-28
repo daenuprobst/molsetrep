eafb8c6a8261e55d51894be306bc64e3f2744af3
Daniel Probst <daenuprobst@gmail.com>
2023-08-28 12:42:37 +0200

    rxn changes

--- run_rxn.sh
+++ run_rxn.sh
@@ -5,14 +5,14 @@
 # python scripts/molnet_test_runner.py doyle msr2 --monitor loss --splitter scaffold --n 10 --max-epochs 450 --project rxn-final --n-hidden-sets 64 --n-hidden-sets 64 --n-elements 4 --n-elements 4 --batch-size 16 --split-ratio 0.5
 # python scripts/molnet_test_runner.py doyle msr2 --monitor loss --splitter scaffold --n 10 --max-epochs 450 --project rxn-final --n-hidden-sets 64 --n-hidden-sets 64 --n-elements 4 --n-elements 4 --batch-size 16 --split-ratio 0.3
 # python scripts/molnet_test_runner.py doyle msr2 --monitor loss --splitter scaffold --n 10 --max-epochs 450 --project rxn-final --n-hidden-sets 64 --n-hidden-sets 64 --n-elements 4 --n-elements 4 --batch-size 16 --split-ratio 0.2
-# python scripts/molnet_test_runner.py doyle msr2 --monitor loss --splitter scaffold --n 10 --max-epochs 450 --project rxn-final --n-hidden-sets 64 --n-hidden-sets 64 --n-elements 4 --n-elements 4 --batch-size 16 --split-ratio 0.1
+# python scripts/molnet_test_runner.py doyle msr2 --monitor loss --splitter scaffold --n 10 --max-epochs 900 --project rxn-final --n-hidden-sets 64 --n-hidden-sets 64 --n-elements 4 --n-elements 4 --batch-size 16 --split-ratio 0.1 --variant even-more-epochs
 # python scripts/molnet_test_runner.py doyle msr2 --monitor loss --splitter scaffold --n 10 --max-epochs 450 --project rxn-final --n-hidden-sets 64 --n-hidden-sets 64 --n-elements 4 --n-elements 4 --batch-size 16 --split-ratio 0.05
 # python scripts/molnet_test_runner.py doyle msr2 --monitor loss --splitter scaffold --n 10 --max-epochs 450 --project rxn-final --n-hidden-sets 64 --n-hidden-sets 64 --n-elements 4 --n-elements 4 --batch-size 16 --split-ratio 0.025
 
 ############
 # Doyle Test
 ############
-python scripts/molnet_test_runner.py doyle_test msr2 --monitor loss --splitter scaffold --n 12 --max-epochs 450 --project rxn-final --n-hidden-sets 64 --n-hidden-sets 64 --n-elements 4 --n-elements 4 --batch-size 16 --variant corrected
+python scripts/molnet_test_runner.py doyle_test msr2 --monitor loss --splitter scaffold --n 4 --max-epochs 900 --project rxn-final --batch-size 16 --variant corrected-test-2
 
 ############
 # AZ

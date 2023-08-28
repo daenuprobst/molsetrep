cc6a3eeab55aa67d04784cf77d24c5d8f331c5e0
Daniel Probst <daenuprobst@gmail.com>
2023-08-28 12:57:12 +0200

    merge

--- run_ocelot.sh
+++ run_ocelot.sh
@@ -1,12 +1,6 @@
 ############
 # ADME
 ############
-<<<<<<< HEAD
-python scripts/molnet_test_runner.py ocelot srgnn  --monitor loss --splitter scaffold --n 5 --max-epochs 150 --project ocelot-final --n-hidden-sets 128 --n-elements 64 --n-hidden-channels 128 --n-hidden-channels 64 --n-layers 8 --task homo
-python scripts/molnet_test_runner.py ocelot gnn --monitor loss --splitter scaffold --n 5 --max-epochs 150 --project ocelot-final --n-hidden-channels 128 --n-hidden-channels 64 --n-layers 8 --task homo
-python scripts/molnet_test_runner.py ocelot msr1 --monitor loss --splitter scaffold --n 5 --max-epochs 250 --project ocelot-final --n-hidden-sets 64 --n-elements 4 --task homo
-python scripts/molnet_test_runner.py ocelot msr2 --monitor loss --splitter scaffold --n 5 --max-epochs 250 --project ocelot-final --n-hidden-sets 64 --n-hidden-sets 64 --n-elements 4 --n-elements 4 --task homo
-=======
 # python scripts/molnet_test_runner.py ocelot srgnn --monitor loss --splitter scaffold --n 5 --max-epochs 150 --n-hidden-channels 64 --n-hidden-channels 32 --batch-size 128 --project ocelot-final --task vie
 # python scripts/molnet_test_runner.py ocelot srgnn --monitor loss --splitter scaffold --n 5 --max-epochs 150 --n-hidden-channels 64 --n-hidden-channels 32 --batch-size 128 --project ocelot-final --task homo
 # python scripts/molnet_test_runner.py ocelot srgnn --monitor loss --splitter scaffold --n 5 --max-epochs 150 --n-hidden-channels 64 --n-hidden-channels 32 --batch-size 128 --project ocelot-final --task ar1
@@ -28,4 +22,3 @@ python scripts/molnet_test_runner.py ocelot srgnn --monitor loss --splitter scaf
 # python scripts/molnet_test_runner.py ocelot gnn --monitor loss --splitter scaffold --n 5 --max-epochs 150 --project ocelot-final --n-hidden-channels 64 --n-hidden-channels 32 --n-layers 4 --task homo
 # python scripts/molnet_test_runner.py ocelot msr1 --monitor loss --splitter scaffold --n 5 --max-epochs 250 --project ocelot-final --n-hidden-sets 64 --n-elements 4 --task homo
 # python scripts/molnet_test_runner.py ocelot msr2 --monitor loss --splitter scaffold --n 5 --max-epochs 250 --project ocelot-final --n-hidden-sets 64 --n-hidden-sets 64 --n-elements 4 --n-elements 4 --task homo
->>>>>>> b08c3c88662fcc03bcc47b26c5c1e43693d91b32
